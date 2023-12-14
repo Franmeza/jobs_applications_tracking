23608e7877a17009a8279e9c8e7f2ad07bab1611
Franmeza <ing.meza.francisco@gmail.com>
2023-12-14 15:21:42 -0500

    Routes with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/jobs" element={<h1>Jobs</h1>} />
+        <Route path="/add-job" element={<h1>Add Job</h1>} />
+        <Route path="/jobs/:id" element={<h1>Job</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
